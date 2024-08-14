29063b864349cf6aee2e26e5ac39b39f8c0a1204
Celot1979 <zata_k@hotmail.com>
2024-08-14 18:19:38 +0200

    Implementación código del proyecto

--- control_gastos/lib/main.dart
+++ control_gastos/lib/main.dart
@@ -1,125 +1,124 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import "package:flutter/widgets.dart";
+import "package:font_awesome_flutter/font_awesome_flutter.dart";
+//Esté es el esquema base para crear una estructura del proyecto. Después de podrá implementar cada parte del grafismo de la app.
+//Todo lo que lleva => es incial
+void main() => runApp(MyApp());// =>
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{// =>
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return MaterialApp(
-      title: 'Flutter',
+      title: "Flutter Demo",
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // 
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+}// =>
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget{// =>
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  _HomePageState createState() => _HomePageState();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+}// =>
+class _HomePageState extends State<HomePage>{// =>
+  //Función de acción de los botones
+  Widget _bottomAction(IconData icon){
+    return InkWell(
+      child: Padding(
+       padding: const EdgeInsets.all(8.0),
+       child: Icon(icon),
+       ),
+      onTap: () {},
+
+    );
   }
-
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context) {// =>
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      //NavigaitorBar. Barra que saldrá al pie de la App. Es donde realmente vamos dandole forma a la APP
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey[300],
+        //Para darle una muesca en el navigationBar para que envuelva al botón flotante
+        notchMargin: 8.0,
+        shape: CircularNotchedRectangle(),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _bottomAction(FontAwesomeIcons.clockRotateLeft),
+          _bottomAction(FontAwesomeIcons.chartPie),
+          SizedBox(width: 32),
+          _bottomAction(FontAwesomeIcons.wallet),
+          _bottomAction(Icons.settings),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+
+    ),
+    //Botón flotante
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.add),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(100.0),
+      ),
+      backgroundColor: Colors.blueAccent,
+      onPressed: (){
+
+
+    },),
+    //Aquí empezamos a diseñar el cuerpo de la aplicación. 
+    //La aplicación llamará al body desde aquí. Más abajo se crea una función que festionará el cuerpo
+    body: _body(),
+    );// =>
+    
+  }
+
+  // Función delcuerpo
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          _selector(),
+          _expenses(),
+          _graph(),
+          _list(),
+        ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _selector()=> Container();
+  Widget _expenses(){
+    return Column(
+      children: <Widget>[
+        Text("\$2361.41", 
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 16.0,
+          color: Colors.blueGrey,
+          ),),
+
+
+        Text("Total expenses",
+         style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 16.0,
+          color: Colors.blueGrey,
+          ),),
+      ]
+
+    );
+  }
+  Widget _graph()=> Container();
+  Widget _list()=> Container();
+
+
 }
